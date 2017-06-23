5be5b432fb1423d464ae5694ca92f8cdcacbf250
Edoardo Spadoni <edoardo.spadoni@nethesis.it>
2017-06-23 16:59:26 +0200

    handled asterisk empty results

--- root/opt/puppetlabs/puppet/lib/ruby/2.1.0/facter/voips.rb
+++ root/opt/puppetlabs/puppet/lib/ruby/2.1.0/facter/voips.rb
@@ -5,15 +5,17 @@ Facter.add('voips') do
   setcode do
     tmp2 = Facter::Core::Execution.exec('asterisk -rx "database show" | grep \'^/registrar/contact/.*user_agent\' | sed \'s/^\/registrar\/contact\/\([0-9]*\);.*"user_agent":"\([^"]*\)".*/\1 \2/\'')
     agents = {}
-    tmp2.split(/\n/).each do |agent|
-      a = agent.split(' ')
-      agents[a[0]] = a.drop(1).join(' ')
-    end
+    if tmp2
+      tmp2.split(/\n/).each do |agent|
+        a = agent.split(' ')
+        agents[a[0]] = a.drop(1).join(' ')
+      end
 
-    tmp = Facter::Core::Execution.exec('asterisk -rx "database show" | grep \'^/DEVICE/\' | sed \'s/^\/DEVICE\/\([0-9]*\)\/.*/\1/\' | sort -u')
-    voips = []
-    tmp.split(/\n/).each do |voip|
-      voips.push({ "extension" => voip.strip, "useragent" => agents[voip] })
+      tmp = Facter::Core::Execution.exec('asterisk -rx "database show" | grep \'^/DEVICE/\' | sed \'s/^\/DEVICE\/\([0-9]*\)\/.*/\1/\' | sort -u')
+      voips = []
+      tmp.split(/\n/).each do |voip|
+        voips.push({ "extension" => voip.strip, "useragent" => agents[voip] })
+      end
     end
     voips
   end
