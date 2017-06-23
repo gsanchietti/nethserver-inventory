e27bf494af73c01f4ce855adaa3487628bd1791b
Edoardo Spadoni <edoardo.spadoni@nethesis.it>
2017-06-23 17:01:53 +0200

    handled asterisk empty results

--- root/usr/lib/ruby/site_ruby/1.8/facter/voips.rb
+++ root/usr/lib/ruby/site_ruby/1.8/facter/voips.rb
@@ -5,11 +5,13 @@ Facter.add('voips') do
   setcode do
     tmp = Facter::Core::Execution.exec('mysql asterisk -e \'SELECT DISTINCT id FROM sip WHERE id REGEXP "^[0-9]+$";\'')
     voips = []
-    tmp.split(/\n/).each do |voip|
-      if voip != 'id'
-        tmp2 = Facter::Core::Execution.exec('asterisk -rx "sip show peer ' + voip + '" | grep Useragent | cut -d: -f2-')
-        tmp2.split(/\n/).each do |agent|
-          voips.push({ "extension" => voip.strip, "useragent" => agent.strip })
+    if tmp
+      tmp.split(/\n/).each do |voip|
+        if voip != 'id'
+          tmp2 = Facter::Core::Execution.exec('asterisk -rx "sip show peer ' + voip + '" | grep Useragent | cut -d: -f2-')
+          tmp2.split(/\n/).each do |agent|
+            voips.push({ "extension" => voip.strip, "useragent" => agent.strip })
+          end
         end
       end
     end
